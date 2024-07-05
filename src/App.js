40a0d98e96d38d9479c318059f39ec8de19de85c
Marianna <20104120@mail.wit.ie>
2024-07-05 13:06:25 +0100

    added Navbar and Route

--- src/App.js
+++ src/App.js
@@ -25,6 +25,19 @@ const App = () => {
             <div className="w-0 dark:bg-secondary-dark-bg">Sidebar </div>
           )
           }
+          <div className={
+            `dark:bg-main-bg bg-main-bg min-h-screen md:ml-72 w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`
+          }>
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+              Navbar
+            </div>
+          </div>
+
+          <div>
+            <Routes>
+              <Route path="/" element="ECommerce" />
+            </Routes>
+          </div>
         </div>
       </BrowserRouter>
 
