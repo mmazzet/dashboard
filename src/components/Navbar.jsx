2214491535b19d030f76b55385e5204ddf9bea5c
Marianna <20104120@mail.wit.ie>
2024-07-06 22:22:44 +0100

    implemented handleClick navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,7 +27,7 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
 );
 
 const Navbar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick } = useStateContext();
 
   return (
     <div className="flex justify-between p-2 md:mx-6 relative">
@@ -72,6 +72,11 @@ const Navbar = () => {
               className='text-gray-400 text-14' />
           </div>
         </TooltipComponent>
+
+          {isClicked.cart && <Cart />}
+          {isClicked.chat && <Chat />}
+          {isClicked.notification && <Notification />}
+          {isClicked.UserProfile && <UserProfile />}
       </div>
     </div>
 
