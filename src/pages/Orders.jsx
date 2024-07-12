fd8fc398d83ecf38314af6e009d899bc1912ab94
Marianna <20104120@mail.wit.ie>
2024-07-12 11:11:47 +0100

    added data source to grid in orders

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -8,11 +8,14 @@ const Orders = () => {
   return (
     <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
       <Header category="Page" title="Orders" />
-      <GridComponent>
+      <GridComponent
+        id="gridcomp"
+        dataSource={ordersData}
+      >
         <ColumnsDirective>
-          {ordersGrid.map((item,index) => {<ColumnDirective key={index} {...item} />
-
-          })}
+        {ordersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
         </ColumnsDirective>
       </GridComponent>
     </div>
